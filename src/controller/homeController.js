abeeb8728122918bf3eeadb394655aae8ec826e8
ProXBucky <hungnguyenbn002@gmail.com>
2022-11-03 16:14:59 +0700

    Finish edit, delete, create

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -2,22 +2,6 @@ import pool from '../config/connectDB'
 
 let getHomePage = async (req, res) => {
     let data = [];
-    // cach nay dai
-    // connection.query(
-    //     'SELECT * FROM `users` ',
-    //     function async (err, results, fields) {
-    //         results.map((row) => {`
-    //             data.push({
-    //                 id: row.id,
-    //                 email: row.email,
-    //                 address: row.address,
-    //                 firstName: row.firstName,
-    //                 lastName: row.lastName
-    //             })
-    //         });
-    //         return res.render('index.ejs', { dataUser: data });
-    //     })
-
     const [rows, fields] = await pool.execute('SELECT * FROM `users`');
     return res.render('index.ejs', { dataUser: rows });
 }
@@ -27,7 +11,6 @@ let getDetailPage = async (req, res) => {
     let [user] = await pool.execute('SELECT * FROM `users` WHERE `id` = ?', [userId])
     return res.send(JSON.stringify(user[0]))
 }
-
 // SQL insert in w3s
 // INSERT INTO table_name (column1, column2, column3, ...)
 // VALUES (value1, value2, value3, ...);
@@ -37,10 +20,8 @@ let createNewUser = async (req, res) => {
     // Cái trên là viết tắt của      let firstName = req.body.firstName ;
     await pool.execute('INSERT INTO users(firstName, lastName, email , address) VALUES(?,?,?,?)',
         [firstName, lastName, email, address])
-
     return res.redirect("/")     // render lại về trang chủ "/"
 }
-
 
 //DELETE FROM table_name WHERE condition;
 let deleteUser = async (req, res) => {
@@ -48,9 +29,29 @@ let deleteUser = async (req, res) => {
     await pool.execute('DELETE FROM users WHERE id = ?', [userId])
     return res.redirect("/")
 }
+
+let getEditPage = async (req, res) => {
+    let id = req.params.id
+    let [user] = await pool.execute('Select * from users where id = ?', [id])
+
+    return res.render('update.ejs', { dataUser: user })
+}
+
+
+//UPDATE table_name
+// SET column1 = value1, column2 = value2, ...
+// WHERE condition;
+let updateUser = async (req, res) => {
+    let { firstName, lastName, email, address, id } = req.body
+    await pool.execute('UPDATE users SET firstName = ? , lastName = ? , email = ? , address = ? WHERE id = ? ', [firstName, lastName, email, address, id])
+    return res.redirect("/")
+}
+
 module.exports = {
     getHomePage,
     getDetailPage,
     createNewUser,
-    deleteUser
+    deleteUser,
+    getEditPage,
+    updateUser
 }
